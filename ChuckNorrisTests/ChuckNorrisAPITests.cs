a7670d6b58fd676a20d471b27616c5e337902471
umakarthikr <100000716+umakarthikr@users.noreply.github.com>
2022-02-20 18:18:05 +0000

    * ChuckNorrisAPITests.cs: Test files
    * ChuckNorrisTests.csproj:

---
+++ ChuckNorrisTests/ChuckNorrisAPITests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+
+namespace ChuckNorrisTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Should_respond_ok_with_joke_requests()
+        {
+            var mockHandler = new MockHttpMessageHandler();
+
+            mockHandler
+                .When("https://api.chucknorris.io/jokes/random")
+                .Respond("application/json", "{'status' : 'OK'}");
+
+            var result = new HttpClient(mockHandler).GetAsync("https://api.chucknorris.io/jokes/random").Result;
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, result.StatusCode);
+
+        }
+    }
+}
